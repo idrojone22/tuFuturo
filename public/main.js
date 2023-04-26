d9b438a1ee8cca6085ed0c2c478a1bdceb16ebec
Jordi <jordivallsplasmx@gmail.com>
2023-04-26 12:19:32 +0200

    Pagina principal

--- public/main.js
+++ public/main.js
@@ -1,5 +1,12 @@
+// Menu
 
-
-let prova = document.getElementsByClassName('nav_a');
-console.log(prova);
+function updatemenu() {
+    if (document.getElementById('responsive-menu').checked == true) {
+      document.getElementById('menu').style.borderBottomRightRadius = '0';
+      document.getElementById('menu').style.borderBottomLeftRadius = '0';
+    } else {
+      document.getElementById('menu').style.borderRadius = '9px';
+    }
+}
+  
 
