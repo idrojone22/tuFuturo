d07003a39bc35f145ec3058123234c9fa15fa665
Jordi <jordivallsplasmx@gmail.com>
2023-04-27 12:54:05 +0200

    Version 1.3.0

--- public/main.js
+++ public/main.js
@@ -1,32 +1,149 @@
 // BD
-
+let arrayataudes = [1, 2, 3, 4];
 let  informacionAtaudes = [
-  {
-    id: 1,
-    precio: 350,
-    nombre: 'Básico',
-    descripcion: ''
-  },
-  {
-    id: 2,
-    precio: 400,
-    nombre: 'Infantil',
-    descripcion: ''
-  },
-  {
-    id: 3,
-    precio: 600,
-    nombre: 'Adulto',
-    descripcion: ''
-  },
-  {
-    id: 4,
-    precio: 1000,
-    nombre: 'Senior',
-    descripcion: ''
-  }
+    {
+        id: 1,
+        precio: 350,
+        nombre: 'Básico',
+        img:  '../imagenes/ataudes0.png',
+        descripcion: ''
+    },
+    {
+        id: 2,
+        precio: 400,
+        nombre: 'Infantil',
+        img:  '../imagenes/ataudes1.png',
+        descripcion: ''
+    },
+    {
+        id: 3,
+        precio: 600,
+        nombre: 'Adulto',
+        img:  '../imagenes/ataudes2.png',
+        descripcion: ''
+    },
+    {
+        id: 4,
+        precio: 1000,
+        nombre: 'Senior',
+        img:  '../imagenes/ataudes3.png',
+        descripcion: ''
+    },
+    {
+        id: 5,
+        precio: 350,
+        nombre: 'Básico',
+        img:  '../imagenes/ataudes4.jpeg',
+        descripcion: ''
+    },
+    {
+        id: 6,
+        precio: 400,
+        nombre: 'Infantil',
+        img:  '../imagenes/ataudes5.jpeg',
+        descripcion: ''
+    },
+    {
+        id: 7,
+        precio: 600,
+        nombre: 'Adulto',
+        img:  '../imagenes/ataudes6.jpg',
+        descripcion: ''
+    },
+    {
+        id: 8,
+        precio: 1000,
+        nombre: 'Senior',
+        img:  '../imagenes/ataudes6.jpg',
+        descripcion: ''
+    },
+    {
+        id: 9,
+        precio: 600,
+        nombre: 'Adulto',
+        img:  '../imagenes/ataudes6.jpg',
+        descripcion: ''
+    },
+    {
+        id: 10,
+        precio: 1000,
+        nombre: 'Senior',
+        img:  '../imagenes/ataudes6.jpg',
+        descripcion: ''
+    },
+    {
+        id: 11,
+        precio: 350,
+        nombre: 'Básico',
+        img:  '../imagenes/ataudes0.png',
+        descripcion: ''
+    },
+    {
+        id: 12,
+        precio: 400,
+        nombre: 'Infantil',
+        img:  '../imagenes/ataudes1.png',
+        descripcion: ''
+    },
+    {
+        id: 13,
+        precio: 600,
+        nombre: 'Adulto',
+        img:  '../imagenes/ataudes2.png',
+        descripcion: ''
+    },
+    {
+        id: 14,
+        precio: 1000,
+        nombre: 'Senior',
+        img:  '../imagenes/ataudes3.png',
+        descripcion: ''
+    },
+    {
+        id: 15,
+        precio: 350,
+        nombre: 'Básico',
+        img:  '../imagenes/ataudes4.jpeg',
+        descripcion: ''
+    },
+    {
+        id: 16,
+        precio: 400,
+        nombre: 'Infantil',
+        img:  '../imagenes/ataudes5.jpeg',
+        descripcion: ''
+    },
+    {
+        id: 17,
+        precio: 600,
+        nombre: 'Adulto',
+        img:  '../imagenes/ataudes6.jpg',
+        descripcion: ''
+    },
+    {
+        id: 18,
+        precio: 1000,
+        nombre: 'Senior',
+        img:  '../imagenes/ataudes6.jpg',
+        descripcion: ''
+    },
+    {
+        id: 19,
+        precio: 600,
+        nombre: 'Adulto',
+        img:  '../imagenes/ataudes6.jpg',
+        descripcion: ''
+    },
+    {
+        id: 20,
+        precio: 1000,
+        nombre: 'Senior',
+        img:  '../imagenes/ataudes6.jpg',
+        descripcion: ''
+    }
 ]
 // presentar 
+let body = document.getElementById('body')
 body.innerHTML = `
   <header>
         <div id="logoFoto">
@@ -85,43 +202,30 @@ body.innerHTML = `
     </main>     
 `;
 
-
-
-
 // Menu
 function updatemenu() {
-  if (document.getElementById('responsive-menu').checked == true) {
-    document.getElementById('menu').style.borderBottomRightRadius = '0'; 
-    document.getElementById('menu').style.borderBottomLeftRadius = '0';
-  } else {
-    document.getElementById('menu').style.borderRadius = '9px';
-  }
+    if (document.getElementById('responsive-menu').checked == true) {
+        document.getElementById('menu').style.borderBottomRightRadius = '0'; 
+        document.getElementById('menu').style.borderBottomLeftRadius = '0';
+    } else {
+        document.getElementById('menu').style.borderRadius = '9px';
+    }
 }
 
 // presentar ataudes
 mostrarAtaudes();
 function mostrarAtaudes() {
-  let ataudes = document.getElementById('ataudes');
-  console.log(ataudes);
-  console.log(informacionAtaudes);
-  // informacionAtaudes.forEach( (e) => {
-  //   ataudes.innerHTML += `
-  //   <div id="objeto${informacionAtaudes[e].id}" class="casilla">
-  //     <img height="300px" width="300px" src="img/ataudes${i}.jpg"/>
-  //     <p class="texto">${informacionAtaudes[e].nombre}</p>
-  //     <p class="texto">${informacionAtaudes[e].precio}</p>
-  //     <button onclick="giro(${i})" class="bot" id="boton${informacionAtaudes[e].id}">Añadir al carrito</button>">
-  //   </div>
-  //   `;
-  // });
-  for (let i = 0; i < informacionAtaudes.length; i++) {
-    ataudes.innerHTML += `
-    <div id="objeto${informacionAtaudes[i].id}" class="casilla">
-      <img height="300px" width="300px" src="img/ataudes${i}.jpg"/>
-      <p class="texto">${informacionAtaudes[i].nombre}</p>
-      <p class="texto">${informacionAtaudes[i].precio}</p>
-      <button onclick="giro(${i})" class="bot" id="boton${informacionAtaudes[i].id}">Añadir al carrito</button>">
-    </div>
+    let ataudes = document.getElementById('ataudes');
+    console.log(ataudes);
+    console.log(informacionAtaudes);
+    for (let i = 0; i < informacionAtaudes.length; i++) {
+    ataudes.innerHTML += 
+    `
+        <div id="objeto${informacionAtaudes[i].id}" class="casilla">
+            <img height="300px" width="300px" src="${informacionAtaudes[i].img}"/>
+            <p class="texto">${informacionAtaudes[i].nombre}</p>
+            <p class="texto">${informacionAtaudes[i].precio}</p>
+        </div>
     `;
   }
 }
