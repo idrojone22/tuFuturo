772c9fb79b046b5eb14949108ec051d0e5a7ccd0
idrojone22 <jordivallsplasmx@gmail.com>
2023-05-23 13:18:52 +0200

    tu futuro

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ let body = document.getElementById('body')
 body.innerHTML = `
   <header>
         <div id="logoFoto">
-            <img src="../imagenes/logo(TuFuturo).png">
+            <img src="./imagenes/logo(TuFuturo).png">
         </div>
         <nav class='menu'>
             <input type='checkbox' id='responsive-menu' onclick='updatemenu()'><label></label>
@@ -24,7 +24,7 @@ body.innerHTML = `
     </header>
     <main>
         <section id="inici">
-            <div id="texto">
+            <div class="texto">
                 <p class="mediano">A tu alcance</p>
                 <p class="grande">TU MEJOR DESCANSO</p>
                 <p class="pequeño">Quieres un descanso eterno y seguro? Ven a tu futuro</p>
@@ -32,9 +32,6 @@ body.innerHTML = `
                 <div id="boton_descubrelo_center">
                     <button id="boton_descubrelo"><a href="#Servicios">DESCÚBRELO</a></button>
                 </div>
-            </div>
-            <div id="imagen_ataud_container">
-                <img id="imagen_ataud" src="../imagenes/ataud.png">
             </div>
             <div id="flecha_container">
                 <div class="flechita">&darr;</div>
@@ -66,11 +63,112 @@ body.innerHTML = `
             </div>
             <div id="urnas"></div>
         </section>
+        <section id="Planes">
+            <div class="container">
+                <h2 class="titulo">PLANES</h2>
+            </div>
+            <div class="texto">
+                <p>En esta sección encontraras nuestros planes para tus seres mas queridos.</p>
+            </div>
+        </section>
     </main>     
 `;
 
+ataudes();
+flores();
+urnas();
+function ataudes() {
+    const url = "https://tu-futuro.glitch.me/ataudes";
+
+    fetch(url)
+        .then(response => response.json())
+        .then (data => {
+            console.log(data);
+            for (let i = 0; i < 20; i++){
+                mostrarAtaudes(data[i]);
+            }
+        })
+
+}
+
+function mostrarAtaudes(ataudes) {
+    let ataudesContainer = document.getElementById('ataudes');
+    console.log('hola');
+    ataudesContainer.innerHTML += `
+        <div id="objeto${ataudes.id}" class="casilla">
+            <img height="300px" width="300px" src="${ataudes.img}"/>
+            <p class="texto">${ataudes.nombre}</p>
+            <p class="texto">${ataudes.precio}</p>
+        </div>
+    `;
+}
+
+function flores() {
+    const url = "https://tu-futuro.glitch.me/flores";
+
+    fetch(url)
+        .then(response => response.json()) 
+        .then (data => {
+            console.log(data);
+            for (let i = 0; i < 20; i++){
+                mostrarFlores(data[i]);
+            }
+        })
+}
+
+function mostrarFlores(flores) {
+    let floresContainer = document.getElementById('flores');
+    floresContainer.innerHTML += `
+        <div id="objeto${flores.id}" class="casilla">
+            <img height="300px" width="300px" src="${flores.img}"/>
+            <p class="texto">${flores.nombre}</p>
+            <p class="texto">${flores.precio}</p>
+        </div>
+    `;
+}
+
+function urnas() {
+    const url = "https://tu-futuro.glitch.me/urnas";
+
+    fetch(url)
+        .then(response => response.json()) 
+        .then (data => {
+            console.log(data);
+            for (let i = 0; i < 20; i++){
+                mostrarUrnas(data[i]);
+            }
+        })
+}
+
+function mostrarUrnas(urnas) {
+    let urnasContainer = document.getElementById('urnas');
+    urnasContainer.innerHTML += `
+        <div id="objeto${urnas.id}" class="casilla">
+            <img height="300px" width="300px" src="${urnas.img}"/>
+            <p class="texto">${urnas.nombre}</p>
+            <p class="texto">${urnas.precio}</p>
+        </div>
+    `;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // fetch
-fetchDB();
+/*fetchDB();
 function fetchDB() {
 
     const url = 'http://192.168.21.105:3000/posts'
@@ -92,21 +190,6 @@ function fetchDB() {
                 mostrarUrnas(data[i]);
             }
         })
-}
-
-function post() {
-    fetch('http://192.168.21.105:3000/posts', {
-            method: 'POST',
-            body: JSON.stringify({
-            title: 'foo',
-            body: 'bar'
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
 }
 
 function mostrarAtaudes(ataudes) {
@@ -140,7 +223,7 @@ function mostrarUrnas(urnas) {
             <p class="texto">${urnas.precio}</p>
         </div>
     `;
-}
+}*/
 
 // Menu
 function updatemenu() {
